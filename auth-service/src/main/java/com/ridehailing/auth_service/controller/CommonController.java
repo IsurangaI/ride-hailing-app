ec7c31f8dae03f086446622afb655b26190c2af2
IsurangaI <isuranga.iniyage@gmail.com>
2026-06-20 09:22:13 +0530

    fix auth service common controller

--- auth-service/src/main/java/com/ridehailing/auth_service/controller/CommonController.java
+++ auth-service/src/main/java/com/ridehailing/auth_service/controller/CommonController.java
@@ -1,9 +1,11 @@
 package com.ridehailing.auth_service.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@RestController("api/auth")
+@RestController
+@RequestMapping("/api/auth")
 public class CommonController {
     @GetMapping("/health")
     public String healthCheck() {
