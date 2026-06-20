6e1c7ebdda5d3dd9b222fae747c2c9fee022933e
IsurangaI <isuranga.iniyage@gmail.com>
2026-06-20 09:23:12 +0530

    setup health check

---
+++ booking-service/src/main/java/com/ridehailing/booking_service/controller/CommonController.java
@@ -0,0 +1,14 @@
+package com.ridehailing.booking_service.controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@RestController
+@RequestMapping("api/bookings")
+public class CommonController {
+    @GetMapping("/health")
+    public String healthCheck() {
+        return "Booking Service is up and running";
+    }
+
+}
