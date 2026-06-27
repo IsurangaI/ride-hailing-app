f17c05cf693d186106b7b9f93e73fa1e7967456b
IsurangaI <isuranga.iniyage@gmail.com>
2026-06-27 19:40:45 +0530

    add rejectedDrivers list to RideRequestedEvent

--- booking-service/src/main/java/com/ridehailing/booking_service/model/event/RideRequestedEvent.java
+++ booking-service/src/main/java/com/ridehailing/booking_service/model/event/RideRequestedEvent.java
@@ -1,10 +1,15 @@
 package com.ridehailing.booking_service.model.event;
 
+import jakarta.persistence.CollectionTable;
+import jakarta.persistence.Column;
+import jakarta.persistence.ElementCollection;
+import jakarta.persistence.JoinColumn;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Data
 @Builder
@@ -17,5 +22,10 @@ public class RideRequestedEvent {
     private Double destinationLongitude;
     private Double destinationLatitude;
     private LocalDateTime requestedAt;
+
+    @ElementCollection
+    @CollectionTable(name = "booking_rejected_drivers", joinColumns = @JoinColumn(name = "booking_id"))
+    @Column(name = "driver_id")
+    private List<String> rejectedDrivers; //the blacklist
 
 }
