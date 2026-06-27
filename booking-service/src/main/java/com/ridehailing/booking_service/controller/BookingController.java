7c2ef408c215e3ca98305eb469b32cd4e8225326
IsurangaI <isuranga.iniyage@gmail.com>
2026-06-27 13:07:13 +0530

    pollBookingStatus controller

--- booking-service/src/main/java/com/ridehailing/booking_service/controller/BookingController.java
+++ booking-service/src/main/java/com/ridehailing/booking_service/controller/BookingController.java
@@ -1,15 +1,13 @@
 package com.ridehailing.booking_service.controller;
 
 
+import com.ridehailing.booking_service.constants.RideStatus;
 import com.ridehailing.booking_service.model.request.BookingRequest;
 import com.ridehailing.booking_service.service.BookingService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/v1/bookings")
@@ -23,6 +21,15 @@ public class BookingController {
         String bookingId = bookingService.createBooking(bookingRequest);
         return ResponseEntity.status(HttpStatus.CREATED).body(bookingId);
     }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<RideStatus> pollBookingStatus(@PathVariable Long id) {
+        RideStatus status = bookingService.getBookingStatus(id);
+        return ResponseEntity.ok(status);
+    }
+
+
+
 
 
 }
