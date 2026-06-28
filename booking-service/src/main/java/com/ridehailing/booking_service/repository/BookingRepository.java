8335acb5dd82014bb29235307566f393a0466813
IsurangaI <isuranga.iniyage@gmail.com>
2026-06-28 10:07:00 +0530

    find expired offers in booking repository

--- booking-service/src/main/java/com/ridehailing/booking_service/repository/BookingRepository.java
+++ booking-service/src/main/java/com/ridehailing/booking_service/repository/BookingRepository.java
@@ -3,14 +3,24 @@ package com.ridehailing.booking_service.repository;
 
 import com.ridehailing.booking_service.model.Booking;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.time.Instant;
+import java.util.List;
 
 @Repository
 public interface BookingRepository extends JpaRepository<Booking, Long> {
 
+    //JPQL method
+    @Query("SELECT b FROM Booking b WHERE b.status = :status AND b.updatedAt < :cutoffTime")
+    List<Booking> findExpiredOffers(
+            @Param("status") String status,
+            @Param("cutoffTime") Instant cutoffTime
+    );
 
-    //@Transactional
-    // write the booking to the bookings table
-    //write to outbox table
+    //JPA method
+    List<Booking> findByStatusAndUpdatedAtBefore(String status, Instant cutoffTime);
 
 }
