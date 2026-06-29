a7a3ed032a6e39209e9fb5612af51d8f43c4b95f
IsurangaI <isuranga.iniyage@gmail.com>
2026-06-29 16:55:56 +0530

    protect routs

--- dashboard-ui/src/App.tsx
+++ dashboard-ui/src/App.tsx
@@ -1,15 +1,11 @@
 import DriverDashboard from "./pages/DriverDashboard";
 import Login from "./pages/Login";
+import Register from "./pages/Register";
 import PassengerDashboard from "./pages/PassengerDashboard";
 import { useAuthStore } from "./store/useAuthStore";
-import { BrowserRouter, Navigate, Route, Router, Routes } from "react-router-dom";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 
-
-
-
-
-// A simple wrapper to protect routes
-const ProtectedRoute = ({ children, allowedRole }: { children: JSX.Element, allowedRole: string }) => {
+const ProtectedRoute = ({ children, allowedRole }: { children: React.ReactNode, allowedRole: string }) => {
   const { token, role } = useAuthStore();
   if (!token) return <Navigate to="/login" />;
   if (role !== allowedRole) return <Navigate to="/unauthorized" />;
@@ -20,14 +16,16 @@ export default function App() {
   return (
       <BrowserRouter>
           <Routes>
+              <Route path="/" element={<Navigate to="/login" />} />
               <Route path="/login" element={<Login />} />
-              
+              <Route path="/register" element={<Register />} />
+
               <Route path="/rider" element={
                   <ProtectedRoute allowedRole="PASSENGER">
                       <PassengerDashboard />
                   </ProtectedRoute>
               } />
-              
+
               <Route path="/driver" element={
                   <ProtectedRoute allowedRole="DRIVER">
                       <DriverDashboard />
