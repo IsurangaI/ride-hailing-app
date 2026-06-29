940737d6c522de645af2a092d037a0da8daa8b2d
IsurangaI <isuranga.iniyage@gmail.com>
2026-06-29 16:56:26 +0530

    Passenger Dashboard scaffold

--- dashboard-ui/src/pages/PassengerDashboard.tsx
+++ dashboard-ui/src/pages/PassengerDashboard.tsx
@@ -1,19 +1,21 @@
-
-
-
-
-
-
-
-
-
-
+import { useNavigate } from 'react-router-dom';
+import { useAuthStore } from '../store/useAuthStore';
 
 function PassengerDashboard() {
-    return (
-        <div> Rider Dashboard </div>
-    )
-}
+    const { logout } = useAuthStore();
+    const navigate = useNavigate();
 
+    const handleLogout = () => {
+        logout();
+        navigate('/login');
+    };
+
+    return (
+        <div>
+            <h2>Rider Dashboard</h2>
+            <button onClick={handleLogout}>Logout</button>
+        </div>
+    );
+}
 
 export default PassengerDashboard;
