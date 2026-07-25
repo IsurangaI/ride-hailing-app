99290471dc3415e341635f252bde1a58c9f5703d
IsurangaI <isuranga.iniyage@gmail.com>
2026-07-25 12:56:57 +0530

    Fix annotation in RideOfferedEvent DTO to support jackson

--- matching-service/src/main/java/com/ridehailing/matchingservice/model/event/RideOfferedEvent.java
+++ matching-service/src/main/java/com/ridehailing/matchingservice/model/event/RideOfferedEvent.java
@@ -2,9 +2,13 @@ package com.ridehailing.matchingservice.model.event;
 
 
 import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class RideOfferedEvent {
     private Long bookingId;
-    private String closestDriverId;
+    private String driverId;
 }
